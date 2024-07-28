6ffe19c693056753d1f1b534ab47968fac74e52f
RB-Pyro <113865828+RB-Pyro@users.noreply.github.com>
2024-07-28 04:04:14 +0000

    ds

--- GUI.py
+++ GUI.py
@@ -2,28 +2,49 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QTabWidget, QMainWindow, QSpacerItem, QSizePolicy, QLabel
 
 class Tab1(QWidget):
-    def __init__(self):
+    def __init__(self, main_window):
         super().__init__()
+        self.main_window = main_window
         layout = QVBoxLayout(self)
         label = QLabel("This is Tab 1", self)
         layout.addWidget(label)
+        back_button = QPushButton('Back to Main', self)
+        back_button.clicked.connect(self.go_back)
+        layout.addWidget(back_button)
         self.setLayout(layout)
 
+    def go_back(self):
+        self.main_window.go_back()
+
 class Tab2(QWidget):
-    def __init__(self):
+    def __init__(self, main_window):
         super().__init__()
+        self.main_window = main_window
         layout = QVBoxLayout(self)
         label = QLabel("This is Tab 2", self)
         layout.addWidget(label)
+        back_button = QPushButton('Back to Main', self)
+        back_button.clicked.connect(self.go_back)
+        layout.addWidget(back_button)
         self.setLayout(layout)
 
+    def go_back(self):
+        self.main_window.go_back()
+
 class Tab3(QWidget):
-    def __init__(self):
+    def __init__(self, main_window):
         super().__init__()
+        self.main_window = main_window
         layout = QVBoxLayout(self)
         label = QLabel("This is Tab 3", self)
         layout.addWidget(label)
+        back_button = QPushButton('Back to Main', self)
+        back_button.clicked.connect(self.go_back)
+        layout.addWidget(back_button)
         self.setLayout(layout)
+
+    def go_back(self):
+        self.main_window.go_back()
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -34,9 +55,9 @@ class MainWindow(QMainWindow):
         self.setGeometry(100, 100, 1920, 1080)
 
         # Create central widget and set layout
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        vbox_layout = QVBoxLayout(central_widget)
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.vbox_layout = QVBoxLayout(self.central_widget)
 
         # Create buttons
         self.button1 = QPushButton('1', self)
@@ -49,33 +70,42 @@ class MainWindow(QMainWindow):
         self.button3.setFixedSize(200, 50)
 
         # Create layout for buttons and add them
-        button_layout = QHBoxLayout()
-        button_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
-        button_layout.addWidget(self.button1)
-        button_layout.addWidget(self.button2)
-        button_layout.addWidget(self.button3)
-        button_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        self.button_layout = QHBoxLayout()
+        self.button_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        self.button_layout.addWidget(self.button1)
+        self.button_layout.addWidget(self.button2)
+        self.button_layout.addWidget(self.button3)
+        self.button_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
 
         # Create tab widget
         self.tabs = QTabWidget()
 
-        # Add the tabs
-        self.tab1 = Tab1()
-        self.tab2 = Tab2()
-        self.tab3 = Tab3()
+        # Add the layouts to the main layout
+        self.vbox_layout.addLayout(self.button_layout)
+        self.vbox_layout.addWidget(self.tabs)
 
+        # Connect buttons to methods
+        self.button1.clicked.connect(lambda: self.show_tab(0))
+        self.button2.clicked.connect(lambda: self.show_tab(1))
+        self.button3.clicked.connect(lambda: self.show_tab(2))
+
+        # Add the tabs but hide them initially
+        self.tab1 = Tab1(self)
+        self.tab2 = Tab2(self)
+        self.tab3 = Tab3(self)
         self.tabs.addTab(self.tab1, "Tab 1")
         self.tabs.addTab(self.tab2, "Tab 2")
         self.tabs.addTab(self.tab3, "Tab 3")
+        self.tabs.setTabVisible(0, False)
+        self.tabs.setTabVisible(1, False)
+        self.tabs.setTabVisible(2, False)
 
-        # Add layouts to the main layout
-        vbox_layout.addLayout(button_layout)
-        vbox_layout.addWidget(self.tabs)
+    def show_tab(self, index):
+        self.tabs.setTabVisible(index, True)
+        self.tabs.setCurrentIndex(index)
 
-        # Connect buttons to methods
-        self.button1.clicked.connect(lambda: self.tabs.setCurrentWidget(self.tab1))
-        self.button2.clicked.connect(lambda: self.tabs.setCurrentWidget(self.tab2))
-        self.button3.clicked.connect(lambda: self.tabs.setCurrentWidget(self.tab3))
+    def go_back(self):
+        self.tabs.setTabVisible(self.tabs.currentIndex(), False)
 
 # Entry point of the application
 if __name__ == '__main__':
@@ -85,5 +115,6 @@ if __name__ == '__main__':
     mainWin.show()
 
     sys.exit(app.exec_())
+
 
 
