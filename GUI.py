37f878b858adb811b7d25d479489a542783e218d
RB-Pyro <113865828+RB-Pyro@users.noreply.github.com>
2024-08-04 06:54:29 +0000

    s

--- GUI.py
+++ GUI.py
@@ -67,7 +67,7 @@ class Tab2(QWidget):
         main_layout.addLayout(top_layout)
 
         # Define an integer variable
-        self.tab_number = 2  # You can change this to any integer
+        self.tab_number = 1  # You can change this to any integer
 
         # Create a QLabel to display the integer in a box
         label = QLabel(f"Channel #:  {self.tab_number}", self)
