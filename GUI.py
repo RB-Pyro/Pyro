a3f888886dedb1a3d46873cce66847e563b81aa9
RB-Pyro <113865828+RB-Pyro@users.noreply.github.com>
2024-08-18 05:19:53 +0000

    f

--- GUI.py
+++ GUI.py
@@ -13,35 +13,31 @@ class ScrollableItem(QWidget):
         # Set the background color to light blue, text color to black, and add a black border
         self.setStyleSheet("""
             background-color: lightblue;
+            color: black;
             border: 3px solid black;
         """)
 
-        # Create a horizontal box layout for the item
-        hbox_layout = QHBoxLayout(self)
-        hbox_layout.setContentsMargins(10, 0, 10, 0)  # Add horizontal margins
-        hbox_layout.setSpacing(10)  # Add spacing between widgets
-        self.setLayout(hbox_layout)
-
-        # Create a grid layout within the box layout
-        grid_layout = QGridLayout()
-        hbox_layout.addLayout(grid_layout)
+        # Create a grid layout for the widget
+        grid_layout = QGridLayout(self)
+        grid_layout.setContentsMargins(5, 5, 5, 5)
+        grid_layout.setSpacing(10)
 
         # Create and set up the label with black text color
         self.label = QLabel(text, self)
-        self.label.setStyleSheet("color: black; padding: 5px;")
+        self.label.setStyleSheet("padding: 5px;")
         self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         grid_layout.addWidget(self.label, 0, 0)  # Add label to the first column
 
         # Create and set up the edit button
         self.edit_button = QPushButton("Edit", self)
         self.edit_button.setFixedSize(80, 40)  # Set height to 40px and width to 80px
-        grid_layout.addWidget(self.edit_button, 0, 1)  # Add edit button to the second column
+        grid_layout.addWidget(self.edit_button, 0, 1, Qt.AlignRight)  # Add edit button to the second column, aligned right
 
         # Create and set up the delete button
         self.delete_button = QPushButton("Delete", self)
         self.delete_button.setFixedSize(80, 40)  # Set height to 40px and width to 80px
         self.delete_button.clicked.connect(self.delete_item)
-        grid_layout.addWidget(self.delete_button, 0, 2)  # Add delete button to the third column
+        grid_layout.addWidget(self.delete_button, 0, 2, Qt.AlignRight)  # Add delete button to the third column, aligned right
 
         # Make the label span the remaining space
         grid_layout.setColumnStretch(0, 1)
@@ -51,6 +47,7 @@ class ScrollableItem(QWidget):
     def delete_item(self):
         # Remove the widget from its parent
         self.setParent(None)
+
 
 
 
